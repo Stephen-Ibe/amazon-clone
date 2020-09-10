c2f43652e1770da7c143d9df42203e704e3a6ffd
KalsonSaint <saintkalson@gmail.com>
2020-09-10 17:54:54 +0100

    header updated

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyADuXFPvnUWq-KI6JJUp1Zf5ZILjuCeSTE",
+  authDomain: "clone-c5d1a.firebaseapp.com",
+  databaseURL: "https://clone-c5d1a.firebaseio.com",
+  projectId: "clone-c5d1a",
+  storageBucket: "clone-c5d1a.appspot.com",
+  messagingSenderId: "351150099833",
+  appId: "1:351150099833:web:e13b8e9d4371d1517073a8",
+  measurementId: "G-YV4RFHKVCX",
+};
